00c0ce5d877f03828e449b54839f34ae9b37ad7d
http://www.github.com/mimsbot <mimseyfield@gmail.com>
2019-02-25 17:16:13 -0500

    first commit

---
+++ reinforcefeb19.py
@@ -0,0 +1,24 @@
+class Person:
+
+    def __init__(self, name, emotions):
+        self.name = name
+        self.emotions = emotions
+
+    def __str__(self):
+        return
+
+    def feeling(self):
+        for i in emotions:
+            if emotions[i] == 3:
+                print("I'm feeling a high amount of {}.".format(i))
+            elif emotions[i] == 2:
+                print("I'm feeling a medium amount of {}.".format(i))
+            elif emotions[i] == 1:
+                print("I'm feeling a low amount of {}.".format(i))
+
+
+emotions = {'happiness': 3, 'fear': 1, 'sadness': 2}
+
+mims = Person('Mims', emotions)
+
+mims.feeling()
